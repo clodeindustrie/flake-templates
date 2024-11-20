1f80455a94c04a3289b30fb68f77ef35a786541e
clodeindustrie <clodeindustrie@gmail.com>
2024-11-20 12:09:01 +1100

    try better

--- php/flake.nix
+++ php/flake.nix
@@ -1,33 +1,36 @@
 {
-    description = "My flake for php/node projects";
+  description = "My flake for php/node projects";
 
-    inputs = {
-        nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
-        flake-utils = { url = "github:numtide/flake-utils"; };
-        phps.url = "github:fossar/nix-phps";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    phps.url = "github:fossar/nix-phps";
+  };
 
-    outputs = { self, nixpkgs, flake-utils, phps }:
-        flake-utils.lib.eachDefaultSystem (system:
-            let
-                pkgs = import nixpkgs { inherit system; };
-            in rec {
-                  devShell = pkgs.mkShell {
-                      buildInputs = [
-                          pkgs.just
-                          pkgs.nodejs-20_x
-                          # pkgs.phpactor
-                          phps.packages.${system}.php81
-                          phps.packages.${system}.php81.packages.composer
-                      ];
+  outputs = { self, nixpkgs, flake-utils, phps, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nativeBuildInputs = with pkgs; [
+          pkgs.just
+          pkgs.nodejs-20_x
+          phps.packages.${system}.php81
+          phps.packages.${system}.php81.packages.composer
+        ];
+        buildInputs = with pkgs; [];
+      in rec {
 
-                      shellHook = ''
-                        if [ ! -f .envrc ]; then
-                        echo "use flake" >> .envrc
-                        echo "dotenv_if_exists .env" >> .envrc
-                        fi
-                        export PATH="$PWD/node_modules/.bin/:$PATH"
-                      '';
-                  };
-              });
+
+        devShells.default = pkgs.mkShell {
+          inherit nativeBuildInputs buildInputs;
+
+          shellHook = ''
+            if [ ! -f .envrc ]; then
+            echo "use flake" >> .envrc
+            echo "dotenv_if_exists .env" >> .envrc
+            fi
+            export PATH="$PWD/node_modules/.bin/:$PATH"
+            '';
+        };
+      });
 }
